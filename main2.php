c61250467da8f52d061169d8a891d40965de61ba
Muhammad aleef Firdaus <maleef.firdaus@gmail.com>
2022-09-29 12:28:29 +0800

    Update main2.php

--- main2.php
+++ main2.php
@@ -53,8 +53,9 @@
     }
     .artanis h2{
         font-size:2em;
-        font-weight:200;
+        font-weight:500;
         color:#0F3460;
+        
         padding-left:5%;
     }
     .container {
@@ -163,7 +164,7 @@
         <div class="col-9">
             <div class="artanis">
             <!-- <img style="padding-left:5%; padding-top:3%; font-size:50px; text-align:left;" src="AC_logo.png"> -->
-            <h1 style="padding-bottom:0%;">NEVER </h1>
+            <h1 style="padding-bottom:0%;">NEVER STOP</h1>
             <h1 style="padding-top:0%;">EXPLORING</h1>
             <h2>Artanis Cloud is a software development company on Artificial Intelligience.</h2>
             </div>
